fc40e50aefbf2ea8bb901c0997528ee1563ac273
vaishnavi071106 <ms0804@srmist.edu.in>
2026-04-14 12:25:19 +0530

    UC5 - Maintain ordered unique train formation using LinkedHashSet

--- src/com/train/app/TrainConsistManagementApp.java
+++ src/com/train/app/TrainConsistManagementApp.java
@@ -4,47 +4,38 @@ import java.util.*;
 
 public class TrainConsistManagementApp {
 
-    private List<String> passengerBogies;
-    private Set<String> bogieIds;
-    private LinkedList<String> trainConsist; // UC4
+    private LinkedHashSet<String> trainFormation; // UC5
 
     public TrainConsistManagementApp() {
-        passengerBogies = new ArrayList<>();
-        bogieIds = new HashSet<>();
-        trainConsist = new LinkedList<>(); // UC4
+        trainFormation = new LinkedHashSet<>();
     }
 
-    // UC4 Methods
-    public void buildTrainConsist() {
+    // UC5 Logic
+    public void buildFormation() {
 
         // Add bogies
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
 
-        // Insert Pantry at position 2
-        trainConsist.add(2, "Pantry Car");
-
-        // Remove first and last
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
+        // Attempt duplicate
+        trainFormation.add("Sleeper"); // ignored automatically
     }
 
-    public void displayTrainConsist() {
-        System.out.println("Final Train Consist: " + trainConsist);
+    public void displayFormation() {
+        System.out.println("Train Formation: " + trainFormation);
     }
 
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println("UC4 - Train Consist using LinkedList");
+        System.out.println("UC5 - LinkedHashSet Formation");
         System.out.println("=================================\n");
 
         TrainConsistManagementApp app = new TrainConsistManagementApp();
 
-        app.buildTrainConsist();
-        app.displayTrainConsist();
+        app.buildFormation();
+        app.displayFormation();
     }
 }
