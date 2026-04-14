b92e9b903b793f488106a4365c51f80cb502a615
vaishnavi071106 <ms0804@srmist.edu.in>
2026-04-14 12:03:05 +0530

    Initial project setup

--- src/com/train/app/TrainConsistManagementApp.java
+++ src/com/train/app/TrainConsistManagementApp.java
@@ -1,4 +1,20 @@
 package com.train.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("UC1 - Initialize Train Consist");
+        System.out.println("====================================\n");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+
+        System.out.println("Train Consist Initialized Successfully.");
+    }
 }
